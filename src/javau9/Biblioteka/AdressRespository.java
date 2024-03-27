5d126f53049672d21e6fc80d24a68a5176f43380
EimantasSimulynas <165022923+EimantasSimulynas@users.noreply.github.com>
2024-03-27 07:51:11 +0200

    Update AdressRespository.java

--- src/javau9/Biblioteka/AdressRespository.java
+++ src/javau9/Biblioteka/AdressRespository.java
@@ -2,7 +2,6 @@
 package.javau9.Biblioteka
 
 public class AdressRespository {
-    import lt.mark3r.biblioteka.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
     public interface AddressRepository extends JpaRepository<Adress.Address, Long> {
